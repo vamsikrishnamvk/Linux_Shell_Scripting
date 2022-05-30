a112cd6ac5e92202042ab78d19918a2b2c9b98a2
vamsikrishnamvk <96385160+vamsikrishnamvk@users.noreply.github.com>
2022-05-30 08:40:29 -0400

    Create script1.sh

---
+++ script1.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+mkdir test2
+cd test2
+touch test21.txt test22.txt test23.txt
+mv test21.txt test21_2022-05-26.txt
+mv test22.txt test22_2022-05-26.txt
+mv test23.txt test23_2022-05-26.txt
+ls
