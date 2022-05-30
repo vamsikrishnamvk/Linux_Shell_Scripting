d8062bfcb7efc4564037fff24c2909f28cf57cd7
vamsikrishnamvk <96385160+vamsikrishnamvk@users.noreply.github.com>
2022-05-30 09:44:16 -0400

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -2,7 +2,7 @@
 mkdir test2
 cd test2
 touch test21.txt test22.txt test23.txt
-mv test21.txt test21_2022-05-26.txt
-mv test22.txt test22_2022-05-26.txt
-mv test23.txt test23_2022-05-26.txt
+mv test21.txt test21_`date "+%Y-%m-%d"`.txt
+mv test22.txt test22_`date "+%Y-%m-%d"`.txt
+mv test23.txt test23_`date "+%Y-%m-%d"`.txt
 ls
